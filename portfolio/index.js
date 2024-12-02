d209b61f6b8899ca9aa79ed5d2ad65ff531cb64a
Ashutosh kumar <ashutoshkr042@gmail.com>
2024-12-02 20:07:02 +0530

    mail fixed

--- portfolio/index.js
+++ portfolio/index.js
@@ -15,15 +15,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
-}));
+app.use(cors());
+
 app.use(express.json()); // Use express's built-in JSON parser
 
 // Nodemailer setup
